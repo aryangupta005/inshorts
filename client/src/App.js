2289098b2fee1fc11024782a88471c78632676f3
Aryan Gupta <aryangupta005@gmail.com>
2024-03-03 10:32:30 +0530

    Inshorts project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Articles from './components/Articles';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Articles />
+    </div>
+  );
+}
+
+export default App;
